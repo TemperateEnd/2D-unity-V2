a0162749d1e6bf57b899e672ae0b6a63006e9835
Reece Tarrant <rtarrant70998@gmail.com>
2019-11-07 14:39:10 +0000

    Revert "Revert "Initial commit""
    This reverts commit 941663f4919dc6a85e0cfa1fa474564c5b89d69e.

---
+++ Assets/Scripts/Gameplay/EnemySpawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public float spawnTimer;
+    public float currentTime;
+    public GameObject enemy;
+    public static GameObject enemyInstance;
+
+    void Start()
+    {
+        currentTime = spawnTimer;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+
+        if(currentTime <= 0)
+        {
+            SpawnEnemies();
+            currentTime = spawnTimer;
+        }
+    }
+
+    void SpawnEnemies()
+    {
+        enemyInstance = Instantiate(enemy, this.transform.position, this.transform.rotation);
+    }
+}
