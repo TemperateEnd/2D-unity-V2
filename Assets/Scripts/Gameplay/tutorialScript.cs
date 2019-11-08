f373ac03d9cea4e36902b3a7af3257c41a96eb62
Reece Tarrant <rtarrant70998@gmail.com>
2019-11-08 18:20:37 +0000

    2d Unity Zombie Game v1.75
    Implemented tutorial content

---
+++ Assets/Scripts/Gameplay/tutorialScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tutorialScript : MonoBehaviour
+{
+    public Canvas tutorialUI;
+    public Text tutorialText;
+    public string tutorialString;
+    public Button closeBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tutorialUI.enabled = false;
+        closeBtn.onClick.AddListener(CloseBtn_OnClick);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Time.timeScale = 0;
+        tutorialUI.enabled = true;
+        tutorialText.text = tutorialString;
+    }
+
+    void CloseBtn_OnClick()
+    {
+        tutorialUI.enabled = false;
+        Time.timeScale = 1;
+        //this.gameObject.SetActive(false);
+    }
+}
